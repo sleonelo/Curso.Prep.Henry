e1a507ae48e193e1e3edc1a722db7ff7f4c6ca88
sleonelo <olmoscarrizosilvioleonel@gmail.com>
2021-12-29 19:29:54 -0300

    v1.5

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -155,8 +155,10 @@ function esPrimo(numero) {
   // Nota: Los números 0 y 1 NO son considerados números primos
   if(numero<=1){
     return false;
+  }else if (numero===2){
+    return true;
   }
-  for(var i= 2 ; i=numero-1 ; i++)
+  for(var i= 2 ; i<numero ; i++)
   if (numero % i === 0){
     return false;
   }else return true;
