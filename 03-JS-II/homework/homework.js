132bc452edb5ec375d4392c7e9aa0d66d1e6022a
sleonelo <olmoscarrizosilvioleonel@gmail.com>
2021-12-29 17:44:13 -0300

    v1.2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -17,10 +17,11 @@ function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
-  if (edad < 18){
-    return "not allowed";
+  if (edad >= 18){
+    return "Allowed";
+  } else {
+    return "Not allowed";
   }
-  return "allowed";
 }
   
 function conection(status) {
@@ -29,6 +30,13 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if(status===1){
+    return "Online";
+  } else if (status===2){
+    return "Away";
+  } else {
+    return "Offline";
+  }
 }
 
 function saludo(idioma) {
@@ -38,6 +46,13 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (saludo==="aleman"){
+    return "Guten Tag!";}
+  else if (saludo==="mandarin"){
+    return "Ni Hao!";}
+  else if (saludo==="ingles"){
+    return "Hello!";}
+  else return  "Hola!";  
 }
 
 function colors(color) {
