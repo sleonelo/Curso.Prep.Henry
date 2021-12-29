0f08965bb054dac096239d0110ff8ef5f36ad584
sleonelo <olmoscarrizosilvioleonel@gmail.com>
2021-12-29 19:24:15 -0300

    v1.4

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -122,9 +122,9 @@ function fizzBuzz(numero) {
   if (numero%3===0 && numero%5===0){
     return "fizzbuzz";}
   else if (numero%5===0){
-    return "fizz";
-  } else if (numero%3===0){
     return "buzz";
+  } else if (numero%3===0){
+    return "fizz";
   }else return numero;
 }
 
@@ -135,7 +135,16 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  
+if (num1<0 || num2<0 || num3<0){
+  return "Hay negativos";
+}else if (num1===0 || num2===0 || num3===0){
+  return "Error";
+}else if (num1>num2 && num1>num3 && num1>0){
+  return "Número 1 es mayor y positivo";
+}else if (num3>num1 && num3>num2){
+  return num3 + 1;
+}
+
 }
 
 function esPrimo(numero) {
@@ -144,6 +153,13 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if(numero<=1){
+    return false;
+  }
+  for(var i= 2 ; i=numero-1 ; i++)
+  if (numero % i === 0){
+    return false;
+  }else return true;
 }
 
 function esVerdadero(valor){
