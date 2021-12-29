b985dd7f05190aae8b74b5e0b9de48eaceabcec4
sleonelo <olmoscarrizosilvioleonel@gmail.com>
2021-12-29 19:36:17 -0300

    v1.6

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -143,7 +143,7 @@ if (num1<0 || num2<0 || num3<0){
   return "Número 1 es mayor y positivo";
 }else if (num3>num1 && num3>num2){
   return num3 + 1;
-}
+}else return false;
 
 }
 
@@ -153,7 +153,7 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if(numero<=1){
+  if(numero<2){
     return false;
   }else if (numero===2){
     return true;
